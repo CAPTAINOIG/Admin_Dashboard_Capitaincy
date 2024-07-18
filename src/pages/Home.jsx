6e00ca1cb16da24707cc4725615c0735aff0790d
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-07-18 17:55:02 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import Card from '../component/DashboardCard/Card'
 import CurrentRevenue from '../Revenue/CurrentRevenue'
 import ProfitChart from '../Profit/ProfitChart'
-import CardOne from '../component/DashboardCard/CardOne'
-import CardTwo from '../component/DashboardCard/CardTwo'
+import MapOne from '../component/Maptable/MapOne'
+import Analytics from '../component/Maptable/Analytics'
 
 const Home = () => {
   return (
     <div className='lg:ms-[22%]'>
         <Card/>
-        {/* <div className='flex'>
-        </div> */}
         <div className='lg:flex md:flex'>
         <CurrentRevenue/>
         <ProfitChart/>
+        </div>
+        <div className='lg:flex md:flex'>
+        <Analytics/>
+        <MapOne/>
         </div>
     </div>
   )
