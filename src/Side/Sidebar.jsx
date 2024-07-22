d287020c99b5c098f3aeb6d62554724c2c6c351a
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-07-22 18:06:45 +0100

    commit

--- src/Side/Sidebar.jsx
+++ src/Side/Sidebar.jsx
@@ -9,17 +9,21 @@ import avatar2 from '../assets/image/avatar2.jpg'
 import { AppContext } from "../context/Dashboard";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
-
-
-
-
-
-
+import { SlCalender } from "react-icons/sl";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineApproval } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { FaTable } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { LiaSignOutAltSolid } from "react-icons/lia";
 
 const Sidebar = () => {
     const { sideBarOpen, setSideBarOpen, isTablet } = useContext(AppContext)
     const [host, setHost] = useState(false)
-    console.log(host);
+    // console.log(host);
+    const [form, setForm] = useState(false)
+    console.log(form);
+
     // check on useRef
     const sidebarRef = useRef();
     // this is useParams
@@ -86,6 +90,14 @@ const Sidebar = () => {
         setHost(true)
     }
 
+    const formToggleUp = () => {
+        setForm(false)
+    }
+
+    const formToggleDown = () => {
+        setForm(true)
+    }
+
 
     return (
         <div className="absolute">
@@ -123,43 +135,57 @@ const Sidebar = () => {
                         </li>
                         <li>
                             <NavLink to={"/layout/calender"} className="link mt-[-30px] hover:bg-gray-500 rounded p-1 flex gap-8 hover:text-white  font-semibold">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <SlCalender size={23} className="mt-1 min-w-max" />
                                 Calender
                             </NavLink>
                         </li>
                         <li>
                             <NavLink to={"/layout/profile"} className="link mt-[-10px] flex gap-8 hover:bg-gray-500 rounded p-1  hover:text-white  font-semibold ">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <CgProfile size={23} className="mt-1 min-w-max" />
                                 Profile
                             </NavLink>
                         </li>
                         <li>
                             <NavLink to={"/layout/approval"} className="link mt-[-10px] flex hover:bg-gray-500 rounded p-1  gap-8 hover:text-white font-semibold ">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <MdOutlineApproval size={23} className="mt-1 min-w-max" />
                                 Approvals
                             </NavLink>
                         </li>
-                        <li>
-                            <NavLink to={"/layout/home"} className="link flex mt-[-10px] hover:bg-gray-500 rounded p-1  gap-8 hover:text-white font-semibold ">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                        <li className="flex gap-20 mb-[-20px] hover:bg-gray-500 p-1 rounded">
+                            <NavLink to={"/layout/home"} className="link flex mt-[-10px] gap-8 hover:text-white font-semibold ">
+                                <FaWpforms  size={23} className="mt-1 min-w-max" />
                                 Forms
                             </NavLink>
+                            <p className="cursor-pointer mt-1 hover:text-blue-500">
+                                {form ?
+                                    (<span ><IoIosArrowDown onClick={() => formToggleUp()} className='transition-transform duration-500 transform rotate-360'/></span>)
+                                    :
+                                    (<span><IoIosArrowForward onClick={() => formToggleDown()}/></span>)
+                                }
+                            </p>
+                        </li>
+                        <li>
+                            <li>
+                                {form ?(<p className="text-center mt-[-10px] py-5 ">Form Elements</p>): ''}
+                                {form ?(<p className="text-center mt-[-10px] mb-1 ">Form Layouts</p>): ''}
+                            </li>
+                       
                         </li>
                         <li>
                             <NavLink to={"/layout/home"} className="link mt-[-10px] flex hover:bg-gray-500 rounded p-1  gap-8 hover:text-white  font-semibold">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <FaTable size={23} className="mt-1 min-w-max" />
                                 Table
                             </NavLink>
                         </li>
                         <li>
                             <NavLink to={"/layout/home"} className="link mt-[-10px] hover:bg-gray-500 rounded p-1  flex gap-8 hover:text-white  font-semibold">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <CiSettings size={23} className="mt-1 min-w-max" />
                                 Settings
                             </NavLink>
                         </li>
                         <li>
                             <NavLink to={"/layout/home"} className="link mt-[-10px] hover:bg-gray-500 rounded p-1 flex gap-8 hover:text-white font-semibold ">
-                                <AiOutlineAppstore size={23} className="mt-1 min-w-max" />
+                                <LiaSignOutAltSolid size={23} className="mt-1 min-w-max" />
                                 Sign Out
                             </NavLink>
                         </li>
